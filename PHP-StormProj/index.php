dfb1ecb103d71108c92c26a8cac77a45c66a9266
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2021-02-07 21:27:25 +0000

    Created the Home/index page and filled it with all information required
    added the link to the data resource and the project vision statement.

--- PHP-StormProj/index.php
+++ PHP-StormProj/index.php
@@ -6,10 +6,23 @@ require_once 'public/newHeader.php';
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         </head>
             <body>
+            <div class = "container-sm">
+                <h4>⠀</h4>
+            </div>
                 <div class = "container-sm">
                     <h1><p><strong>Welcome!</strong></p></h1>
-                    <h5>This is the home page, although I could not figure out how to make such a title properly display in the header. This is my COMP2001 PHP project webpage!</h5>
+                    <h5>This is the home page. Welcome to my COMP2001 PHP project webpage presenting plymouth landscape character area information!</h5>
                 </div>
+                <div class= "container-sm">
+                    <h4>⠀</h4>
+                <h1><p><strong>Data Source!</strong></p></h1>
+                    <h5>The original data source for the resources used on this side can be found here: <a href="https://plymouth.thedata.place/dataset/plymouth-landscape-character-areas">https://plymouth.thedata.place/dataset/plymouth-landscape-character-areas</a></h5>
+                </div>
+            <div class= "container-sm">
+                <h4>⠀</h4>
+                <h1><p><strong>Project Vision!</strong></p></h1>
+                <h5>This webpage represents gathered data for those looking to find the extend of different landscape areas in plymouth. It's a data resource that converts the raw collected data into something more readable for people to use and understand.</h5>
+            </div>
             </body>
         </html>
 <?php
--- PHP-StormProj/public/footer.php
+++ PHP-StormProj/public/footer.php
@@ -3,18 +3,20 @@
 ?>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
+
 <style>.footer{
         position: fixed;
-        left: 0;
         bottom: 0;
-        width: 100%;
+        left: 15%;
+        width: 70%;
         background-color: #0d6dfc;
         color: white;
         text-align: center;
-        padding: 10px 0px;
-        margin-right: auto;
-        margin-left: auto;
+        padding: 10px;
+        margin: auto;
+
     }
 </style>
     <div class='footer'>
